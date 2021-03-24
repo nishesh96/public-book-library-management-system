db830539cb0dd5b261095c6e5735b594fc484f19
Nishesh <bharech.nishesh96@gmail.com>
2021-03-24 09:37:37 +0530

    added no cors to server

--- server/server.js
+++ server/server.js
@@ -1,24 +1,14 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
-
-
+const express = require("express");
+var cors = require("cors");
 const app = express();
 
+app.use(cors());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(express.json());
+app.use(express.urlencoded());
 
-
-const routes = require('./routes/routes.js')(app)
+const routes = require("./routes/routes.js")(app);
 
 const server = app.listen(3001, () => {
-    console.log('listening on port %s....', server.address().port);
-})
-
-
+  console.log("listening on port %s....", server.address().port);
+});
